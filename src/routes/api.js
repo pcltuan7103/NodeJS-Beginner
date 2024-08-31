0f29376f7fcea414ccaaa944ccc9524f0ebc7eae
tuanpcl7103 <tuanpcl7103.gmail.com>
2024-08-31 19:05:14 +0700

    Middleware in NodeJS

--- src/routes/api.js
+++ src/routes/api.js
@@ -5,7 +5,11 @@ const {
   getUser,
 } = require("../controllers/userController");
 
+const delay = require("../middleware/delay");
+
 const routerAPI = express.Router();
+
+routerAPI.get("*", delay);
 
 routerAPI.get("/", (req, res) => {
   return res.status(200).json("Hello API");
