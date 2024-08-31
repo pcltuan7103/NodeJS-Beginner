b2883ec895715044ed0ead2c112b6a5e07cc590a
tuanpcl7103 <tuanpcl7103.gmail.com>
2024-08-31 14:09:12 +0700

    Create API Login

---
+++ src/services/userService.js
@@ -0,0 +1,56 @@
+const User = require("../models/user");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+
+const createUserService = async (name, email, password) => {
+  try {
+    //hash password
+    const hashPassword = await bcrypt.hash(password, saltRounds);
+    //save to database
+    let result = await User.create({
+      name: name,
+      email: email,
+      password: hashPassword,
+      role: "HOIDANIT",
+    });
+    return result;
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+};
+
+const loginService = async (email, password) => {
+  try {
+    //fetch user by email
+    const user = await User.findOne({ email: email }).exec();
+    if (user) {
+      //compare password
+      const isMatchPassword = await bcrypt.compare(password, user.password);
+      if (!isMatchPassword) {
+        return {
+          EC: 2,
+          EM: "Email/Password is invalid",
+        };
+      } else {
+        //create an access token
+        return "Create an access token";
+      }
+    } else {
+      return {
+        EC: 1,
+        EM: "Email/Password is invalid",
+      };
+    }
+
+    return result;
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+};
+
+module.exports = {
+  createUserService,
+  loginService,
+};
