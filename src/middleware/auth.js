7ac399dc6b16e95f4991b10d6a34444c04ea7777
tuanpcl7103 <tuanpcl7103.gmail.com>
2024-08-31 19:31:54 +0700

    Create JWT Middleware

---
+++ src/middleware/auth.js
@@ -0,0 +1,33 @@
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
+
+const auth = (req, res, next) => {
+  const white_lists = ["/", "/login", "/register"];
+
+  if (white_lists.find((item) => "/v1/api" + item === req.originalUrl)) {
+    next();
+  } else {
+    if (req?.headers?.authorization?.split(" ")?.[1]) {
+      const token = req.headers.authorization.split(" ")[1];
+
+      //Verify token
+      try {
+        const decode = jwt.verify(token, process.env.JWT_SECRET);
+
+        console.log("Check token", decode);
+        next();
+      } catch {
+        return res.status(401).json({
+          message: "Token is expired or invalid",
+        });
+      }
+    } else {
+      return res.status(401).json({
+        message:
+          "You have not passed access token in header/The token has expired!",
+      });
+    }
+  }
+};
+
+module.exports = auth;
--- src/routes/api.js
+++ src/routes/api.js
@@ -6,10 +6,11 @@ const {
 } = require("../controllers/userController");
 
 const delay = require("../middleware/delay");
+const auth = require("../middleware/auth");
 
 const routerAPI = express.Router();
 
-routerAPI.get("*", delay);
+routerAPI.all("*", auth);
 
 routerAPI.get("/", (req, res) => {
   return res.status(200).json("Hello API");
