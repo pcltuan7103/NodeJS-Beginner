3fd8e564c63aeadd90d07e95d5132c617fbe872b
tuanpcl7103 <tuanpcl7103.gmail.com>
2024-08-31 11:55:32 +0700

    Create API Register(Hash Password)

---
+++ src/controllers/userController.js
@@ -0,0 +1,11 @@
+const { createUserService } = require("../services/customerService");
+
+const createUser = async (req, res) => {
+  const { name, email, password } = req.body;
+  const data = await createUserService(name, email, password);
+  return res.status(200).json(data);
+};
+
+module.exports = {
+  createUser,
+};
--- src/services/customerService.js
+++ src/services/customerService.js
@@ -1,27 +1,25 @@
 const User = require("../models/user");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
-const createCustomerService = async (customerData) => {
-    // try {
-    //     let result = await Customer.create({
-    //         name: customerData.name,
-    //         address: customerData.address,
-    //         phone: customerData.phone,
-    //         email: customerData.email,
-    //         description: customerData.description,
-    //         image: customerData.image
-    //     })
-    //     return result;
-
-    // } catch (error) {
-    //     console.log(error);
-    //     return null;
-    // }
-}
-
-
-
-
+const createUserService = async (name, email, password) => {
+  try {
+    //hash password
+    const hashPassword = await bcrypt.hash(password, saltRounds);
+    //save to database
+    let result = await User.create({
+      name: name,
+      email: email,
+      password: hashPassword,
+      role: "HOIDANIT",
+    });
+    return result;
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+};
 
 module.exports = {
-    createCustomerService
-}
+  createUserService,
+};
