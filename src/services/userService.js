2229157cbcf5430721bb75c1a131788cea250783
tuanpcl7103 <tuanpcl7103.gmail.com>
2024-08-31 14:39:44 +0700

    Update Login

--- src/services/userService.js
+++ src/services/userService.js
@@ -44,6 +44,7 @@ const loginService = async (email, password) => {
           expiresIn: process.env.JWT_EXPIRE,
         });
         return {
+          EC: 0,
           access_token,
           user: {
             email: user.email,
